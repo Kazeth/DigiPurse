1022385344d3331fc95ac55eca2c06ee4781e75b
Kazeth <roonysoon88@gmail.com>
2025-07-15 00:02:32 +0700

    Update : DigiDocumentPage
    able to provide & display progress multiple action (download & upload)

--- frontend/src/pages/DigiDocumentPage.jsx
+++ frontend/src/pages/DigiDocumentPage.jsx
@@ -1,7 +1,8 @@
 import { AuthClient } from '@dfinity/auth-client';
-import { createActor } from 'declarations/backend';
+import { createActor } from 'declarations/Document_backend';
 import { canisterId } from 'declarations/backend/index.js';
 import React, { useState, useEffect } from 'react';
+import { Button } from '@/components/ui/button';
 
 const network = process.env.DFX_NETWORK;
 const identityProvider =
@@ -15,7 +16,11 @@ export default function DigiDocumentPage() {
     const [actor, setActor] = useState();
     const [files, setFiles] = useState([]);
     const [errorMessage, setErrorMessage] = useState();
-    const [fileTransferProgress, setFileTransferProgress] = useState();
+    const [filesTransferProgress, setFilesTransferProgress] = useState([]);
+
+    useEffect(() => {
+
+    }, []);
 
     useEffect(() => {
         updateActor();
@@ -43,16 +48,25 @@ export default function DigiDocumentPage() {
         setIsAuthenticated(isAuthenticated);
     }
 
-    async function login() {
-        await authClient.login({
-            identityProvider,
-            onSuccess: updateActor
+    async function updateProgress(fileName, progress, mode) {
+        setFilesTransferProgress((prevList) => {
+            const existingIndex = prevList.findIndex(item => item.fileName === fileName);
+            const updatedItem = { fileName, progress, mode };
+
+            if (existingIndex >= 0) {
+                const newList = [...prevList];
+                newList[existingIndex] = updatedItem;
+                return newList;
+            } else {
+                return [...prevList, updatedItem];
+            }
         });
     }
 
-    async function logout() {
-        await authClient.logout();
-        updateActor();
+    async function removeProgress(fileName) {
+        setFilesTransferProgress((prevList) =>
+            prevList.filter(item => item.fileName !== fileName)
+        );
     }
 
     async function loadFiles() {
@@ -66,60 +80,51 @@ export default function DigiDocumentPage() {
     }
 
     async function handleFileUpload(event) {
-        const file = event.target.files[0];
+        const files = event.target.files;
         setErrorMessage();
 
-        if (!file) {
-            setErrorMessage('Please select a file to upload.');
-            return;
-        }
+        for (const file of files) {
 
-        if (await actor.checkFileExists(file.name)) {
-            setErrorMessage(`File "${file.name}" already exists. Please choose a different file name.`);
-            return;
-        }
-        setFileTransferProgress({
-            mode: 'Uploading',
-            fileName: file.name,
-            progress: 0
-        });
-
-        const reader = new FileReader();
-        reader.onload = async (e) => {
-            const content = new Uint8Array(e.target.result);
-            const chunkSize = 1024 * 1024; // 1 MB chunks
-            const totalChunks = Math.ceil(content.length / chunkSize);
-
-            try {
-                for (let i = 0; i < totalChunks; i++) {
-                    const start = i * chunkSize;
-                    const end = Math.min(start + chunkSize, content.length);
-                    const chunk = content.slice(start, end);
-
-                    await actor.uploadFileChunk(file.name, chunk, BigInt(i), file.type);
-                    setFileTransferProgress((prev) => ({
-                        ...prev,
-                        progress: Math.floor(((i + 1) / totalChunks) * 100)
-                    }));
-                }
-            } catch (error) {
-                console.error('Upload failed:', error);
-                setErrorMessage(`Failed to upload ${file.name}: ${error.message}`);
-            } finally {
-                await loadFiles();
-                setFileTransferProgress(null);
+            if (!file) {
+                continue;
             }
-        };
 
-        reader.readAsArrayBuffer(file);
+            if (await actor.checkFileExists(file.name)) {
+                setErrorMessage(`File "${file.name}" already exists. Skipping the file...`);
+                continue;
+            }
+
+            const reader = new FileReader();
+            reader.onload = async (e) => {
+                const content = new Uint8Array(e.target.result);
+                const chunkSize = 1024 * 1024; // 1 MB chunks
+                const totalChunks = Math.ceil(content.length / chunkSize);
+
+
+                try {
+                    for (let i = 0; i < totalChunks; i++) {
+                        const start = i * chunkSize;
+                        const end = Math.min(start + chunkSize, content.length);
+                        const chunk = content.slice(start, end);
+
+                        await actor.uploadFileChunk(file.name, chunk, BigInt(i), file.type);
+                        updateProgress(file.name, Math.floor(((i + 1) / totalChunks) * 100), 'Uploading');
+                    }
+                } catch (error) {
+                    console.error('Upload failed:', error);
+                    setErrorMessage(`Failed to upload ${file.name}: ${error.message}`);
+                } finally {
+                    await loadFiles();
+                    removeProgress(file.name);
+                }
+            };
+
+            reader.readAsArrayBuffer(file);
+        }
     }
 
     async function handleFileDownload(name) {
-        setFileTransferProgress({
-            mode: 'Downloading',
-            fileName: name,
-            progress: 0
-        });
+        updateProgress(name, 0, 'Downloading');
         try {
             const totalChunks = Number(await actor.getTotalChunks(name));
             const fileType = await actor.getFileType(name)[0];
@@ -133,10 +138,7 @@ export default function DigiDocumentPage() {
                     throw new Error(`Failed to retrieve chunk ${i}`);
                 }
 
-                setFileTransferProgress((prev) => ({
-                    ...prev,
-                    progress: Math.floor(((i + 1) / totalChunks) * 100)
-                }));
+                updateProgress(name, Math.floor(((i + 1) / totalChunks) * 100), 'Downloading');
             }
 
             const data = new Blob(chunks, { type: fileType });
@@ -150,7 +152,7 @@ export default function DigiDocumentPage() {
             console.error('Download failed:', error);
             setErrorMessage(`Failed to download ${name}: ${error.message}`);
         } finally {
-            setFileTransferProgress(null);
+            removeProgress(name);
         }
     }
 
@@ -170,57 +172,70 @@ export default function DigiDocumentPage() {
         }
     }
 
+    async function handleFilePreview(name) {
+
+    }
+
     return (
         <div className="container mx-auto p-4">
-            {!isAuthenticated ? (
-                <div className="mt-4 rounded-md border-l-4 bg-neutral-200 p-4 shadow-md">
-                    <p className="mt-2 text-black">Please sign in to access the file vault.</p>
+            <div>
+                <div className="mb-4">
+                    <input
+                        type="file"
+                        multiple
+                        onChange={handleFileUpload}
+                        className="block w-full text-sm text-gray-500 file:mr-4 file:rounded-full file:border-0 file:bg-blue-50 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-blue-700 hover:file:bg-blue-100"
+                    />
                 </div>
-            ) : (
-                <div>
-                    <div className="mb-4">
-                        <input
-                            type="file"
-                            onChange={handleFileUpload}
-                            className="block w-full text-sm text-gray-500 file:mr-4 file:rounded-full file:border-0 file:bg-blue-50 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-blue-700 hover:file:bg-blue-100"
-                        />
+
+                {errorMessage && (
+                    <div className="mt-4 rounded-md border border-red-400 bg-red-100 p-3 text-red-700">{errorMessage}</div>
+                )}
+
+                {filesTransferProgress.length > 0 && (
+                    <div>
+                        {filesTransferProgress.map((item) => (
+                            <div key={item.fileName}className="mb-4">
+                                <p className="mb-2 text-sm text-white">
+                                    {`${item.mode} ${item.fileName}`}
+                                </p>
+                                <p className='mb-2 text-[2vw] text-white'>
+                                    {`${item.progress} %`}
+                                </p>
+                            </div>
+
+                        ))}
+
                     </div>
+                )}
 
-                    {errorMessage && (
-                        <div className="mt-4 rounded-md border border-red-400 bg-red-100 p-3 text-red-700">{errorMessage}</div>
-                    )}
-
-                    {fileTransferProgress && (
-                        <div className="mb-4">
-                            <p className="mb-2 text-sm text-gray-600">
-                                {`${fileTransferProgress.mode} ${fileTransferProgress.fileName} ... ${fileTransferProgress.progress}%`}
-                            </p>
-                        </div>
-                    )}
-
-                    <div className="space-y-2">
-                        {files.length === 0 ? (
-                            <p className="py-8 text-center text-gray-500">You have no files. Upload some!</p>
-                        ) : (
-                            files.map((file) => (
-                                <div key={file.name} className="flex items-center justify-between rounded-lg bg-white p-3 shadow">
-                                    <div className="flex items-center space-x-2">
-                                        <span>{file.name}</span>
-                                    </div>
-                                    <div className="flex space-x-2">
-                                        <button onClick={() => handleFileDownload(file.name)} className="btn">
-                                            Download
-                                        </button>
-                                        <button onClick={() => handleFileDelete(file.name)} className="btn">
-                                            Delete
-                                        </button>
-                                    </div>
+                <div className="space-y-2">
+                    {files.length === 0 ? (
+                        <p className="py-8 text-center text-gray-500">You have no files. Upload some!</p>
+                    ) : (
+                        files.map((file) => (
+                            <div key={file.name} className="flex items-center justify-between rounded-lg bg-white p-3 shadow text-[#4C1D95]">
+                                <div className="flex items-center space-x-2">
+                                    <span>{file.name}</span>
                                 </div>
-                            ))
-                        )}
-                    </div>
+                                <div className="flex space-x-2">
+                                    <Button onClick={() => handleFilePreview(file.name)} className="btn">
+                                        Preview
+                                    </Button>
+                                    <Button onClick={() => handleFileDownload(file.name)} className="btn">
+                                        Download
+                                    </Button>
+                                    <Button onClick={() => handleFileDelete(file.name)} className="btn">
+                                        Delete
+                                    </Button>
+
+                                </div>
+                            </div>
+                        ))
+                    )}
                 </div>
-            )}
+            </div>
+
         </div>
     );
 }
